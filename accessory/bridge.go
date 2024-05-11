2f60c94965846ba42f54dbf492910f65c2f7b5cf
Jason Lu <ljunq1223@outlook.com>
2024-05-11 16:39:09 +0800

    回退，修复配件设备异常问题

--- accessory/bridge.go
+++ accessory/bridge.go
@@ -1,19 +1,13 @@
 package accessory
 
-import "github.com/LUJUNQUAN/hap/service"
-
 type Bridge struct {
 	*A
-	ServiceLabel *service.ServiceLabel
 }
 
 // NewBridge returns a bridge which implements model.Bridge.
 func NewBridge(info Info) *Bridge {
 	a := Bridge{}
 	a.A = New(info, TypeBridge)
-
-	a.ServiceLabel = service.NewServiceLabel()
-	a.AddS(a.ServiceLabel.S)
 
 	return &a
 }
