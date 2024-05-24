4ac90e2a5e9ccb79a197480d1c07cd0b0143c72f
Jason Lu <ljunq1223@outlook.com>
2024-05-24 09:09:49 +0800

    1.新增窗帘类型带水平角度调整
    2.新增窗帘类型带垂直角度调整

--- cmd/switch/main.go
+++ cmd/switch/main.go
@@ -20,21 +20,75 @@ func main() {
 		Name: "bridge",
 	})
 
-	a.Id = uint64(1)
-
 	b := accessory.NewSwitch(accessory.Info{
 		Name: "Lamp",
 	})
-
-	b.Id = uint64(2)
 
 	d := accessory.NewSwitch(accessory.Info{
 		Name: "Lamp1",
 	})
 
-	d.Id = uint64(3)
+	e := accessory.NewWindowCoveringVertical(accessory.Info{
+		Name: "Vertical",
+	})
 
-	s, err := hap.NewServer(hap.NewFsStore("./db"), a.A, b.A, d.A)
+	f := accessory.NewWindowCoveringHorizontal(accessory.Info{
+		Name: "Horizontal",
+	})
+
+	c1 := accessory.NewSwitch(accessory.Info{
+		Name: "Lamp02341232-2",
+	})
+
+	c2 := accessory.NewSwitch(accessory.Info{
+		Name: "Lamp02341232-3",
+	})
+
+	c3 := accessory.NewSwitch(accessory.Info{
+		Name: "Lamp02341232-4",
+	})
+
+	c4 := accessory.NewSwitch(accessory.Info{
+		Name: "Lamp02341232-5",
+	})
+
+	c5 := accessory.NewSwitch(accessory.Info{
+		Name: "Lamp02341232-6",
+	})
+
+	c6 := accessory.NewSwitch(accessory.Info{
+		Name: "Lamp02341232-7",
+	})
+
+	c7 := accessory.NewSwitch(accessory.Info{
+		Name: "Lamp02341232-8",
+	})
+
+	c8 := accessory.NewSwitch(accessory.Info{
+		Name: "Lamp02341232-9",
+	})
+
+	c9 := accessory.NewSwitch(accessory.Info{
+		Name: "Lamp02341232-10",
+	})
+
+	c10 := accessory.NewSwitch(accessory.Info{
+		Name: "Lamp02341232-11",
+	})
+
+	c11 := accessory.NewSwitch(accessory.Info{
+		Name: "Lamp02341232-12",
+	})
+
+	c12 := accessory.NewSwitch(accessory.Info{
+		Name: "Lamp02341232-13",
+	})
+
+	h := accessory.NewTelevision(accessory.Info{
+		Name: "Television",
+	})
+
+	s, err := hap.NewServer(hap.NewFsStore("./db"), a.A, b.A, d.A, e.A, f.A, c1.A, c2.A, c3.A, c4.A, c5.A, c6.A, c7.A, c8.A, c9.A, c10.A, c11.A, c12.A, h.A)
 	if err != nil {
 		log.Panic(err)
 	}
